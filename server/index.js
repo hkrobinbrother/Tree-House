cafb44b405042c9d8b6661cc73396bab8edb0ed9
HKrobinvai <hkrobin49@gmail.com>
2026-01-04 20:54:15 +0600

    setUp the mongodb atlas

--- server/index.js
+++ server/index.js
@@ -36,7 +36,8 @@ const verifyToken = async (req, res, next) => {
   })
 }
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mq0mae1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.d1icoll.mongodb.net/?appName=Cluster0`;
+console.log(process.env.DB_USER)
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
