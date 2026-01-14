ac62aa1b20890a9285b7456feade6d326cb9c458
HKrobinvai <hkrobin49@gmail.com>
2026-01-14 21:28:24 +0600

    change the code

--- client/src/components/Dashboard/Sidebar/Sidebar.jsx
+++ client/src/components/Dashboard/Sidebar/Sidebar.jsx
@@ -103,14 +103,10 @@ const Sidebar = () => {
           >
             <GrLogout className='w-5 h-5' />
 
-            {/* <span className='mx-4 font-medium'>Logout</span> */}
+            <span className='mx-4 font-medium'>Logout</span>
 
-            <div  className='mx-4 font-medium'>
-              Logout
-            </div>
-            <div  className='mx-4 font-medium'>
-              Logout
-            </div>
+            
+            
           </button>
         </div>
       </div>
